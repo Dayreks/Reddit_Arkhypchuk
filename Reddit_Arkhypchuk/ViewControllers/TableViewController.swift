fc26c3e2f82b0d7a092937d5e77d43918a235b09
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2022-04-14 20:00:02 +0300

    Minor fixes

--- Reddit_Arkhypchuk/ViewControllers/TableViewController.swift
+++ Reddit_Arkhypchuk/ViewControllers/TableViewController.swift
@@ -79,7 +79,6 @@ class TableViewController: UIViewController, UITableViewDataSource, UIScrollView
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell_id", for: indexPath) as! PostCell
         cell.configureView(post: PostRepository.shared.data[indexPath.row])
         cell.delegate = self
-        if (savedActive) {cell.backgroundColor = cell.backgroundColor?.inverseColor()}
         return cell
     }
     
@@ -135,28 +134,28 @@ extension TableViewController: PostCellDelagate {
 }
 
 
-extension UIColor{
-    func inverseColor() -> UIColor {
-        var alpha: CGFloat = 1.0
-
-        var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
-        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
-            return UIColor(red: 1.0 - red, green: 1.0 - green, blue: 1.0 - blue, alpha: alpha)
-        }
-
-        var hue: CGFloat = 0.0, saturation: CGFloat = 0.0, brightness: CGFloat = 0.0
-        if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
-            return UIColor(hue: 1.0 - hue, saturation: 1.0 - saturation, brightness: 1.0 - brightness, alpha: alpha)
-        }
-
-        var white: CGFloat = 0.0
-        if self.getWhite(&white, alpha: &alpha) {
-            return UIColor(white: 1.0 - white, alpha: alpha)
-        }
-
-        return self
-    }
-}
+//extension UIColor{
+//    func inverseColor() -> UIColor {
+//        var alpha: CGFloat = 1.0
+//
+//        var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
+//        if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+//            return UIColor(red: 1.0 - red, green: 1.0 - green, blue: 1.0 - blue, alpha: alpha)
+//        }
+//
+//        var hue: CGFloat = 0.0, saturation: CGFloat = 0.0, brightness: CGFloat = 0.0
+//        if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+//            return UIColor(hue: 1.0 - hue, saturation: 1.0 - saturation, brightness: 1.0 - brightness, alpha: alpha)
+//        }
+//
+//        var white: CGFloat = 0.0
+//        if self.getWhite(&white, alpha: &alpha) {
+//            return UIColor(white: 1.0 - white, alpha: alpha)
+//        }
+//
+//        return self
+//    }
+//}
 
 
 
